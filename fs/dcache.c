fbcdde6df78358b4feaa63e16e0fd222945e6189
Kingslayer9988 <king.slayer@tuta.io>
2025-07-11 11:55:22 +0000

    BACKPORT 1.5.9

--- fs/dcache.c
+++ fs/dcache.c
@@ -2202,11 +2202,6 @@ seqretry:
 				continue;
 			if (dentry_cmp(dentry, str, hashlen_len(hashlen)) != 0)
 				continue;
-#ifdef CONFIG_KSU_SUSFS_SUS_PATH
-			if (dentry->d_inode && unlikely(dentry->d_inode->i_state & INODE_STATE_SUS_PATH) && likely(susfs_is_current_non_root_user_app_proc())) {
-				continue;
-			}
-#endif
 		}
 		*seqp = seq;
 		return dentry;
@@ -2289,12 +2284,6 @@ struct dentry *__d_lookup(const struct dentry *parent, const struct qstr *name)
 
 		if (dentry->d_name.hash != hash)
 			continue;
-
-#ifdef CONFIG_KSU_SUSFS_SUS_PATH
-		if (dentry->d_inode && unlikely(dentry->d_inode->i_state & INODE_STATE_SUS_PATH) && likely(susfs_is_current_non_root_user_app_proc())) {
-			continue;
-		}
-#endif
 
 		spin_lock(&dentry->d_lock);
 		if (dentry->d_parent != parent)
