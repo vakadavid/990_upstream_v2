cfc39ef3eed78a5aef24fc9b54a5c8cc41c73d40
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-10-22 09:31:49 +0200

    Update namespace.c

--- fs/namespace.c
+++ fs/namespace.c
@@ -458,16 +458,6 @@ static inline void mnt_dec_writers(struct mount *mnt)
 #endif
 }
 
-static unsigned int mnt_get_writers(struct mount *mnt)
-{
-#ifdef CONFIG_SMP
-	unsigned int count = 0;
-	int cpu;
-
-	for_each_possible_cpu(cpu) {
-		count += per_cpu_ptr(mnt->mnt_pcp, cpu)->mnt_writers;
-	}
-
 	return count;
 #else
 	return mnt->mnt_writers;
