76eecd029bc1e50f8cedca100cafbc73d4a395aa
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-10-26 11:38:49 +0100

    SUSFS AVC

--- include/linux/susfs.h
+++ include/linux/susfs.h
@@ -196,6 +196,7 @@ int susfs_add_sus_map(struct st_susfs_sus_map* __user user_info);
 #endif
 
 int susfs_get_enabled_features(char __user* buf, size_t bufsize);
+void susfs_set_avc_log_spoofing(bool enabled);
 
 /* susfs_init */
 void susfs_init(void);
