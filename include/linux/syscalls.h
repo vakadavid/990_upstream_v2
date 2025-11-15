bab52dd429fa87e5d3f9a3ded50ec4dcb03b1bcb
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-11-15 09:09:04 +0100

    Update syscalls.h

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -1228,7 +1228,7 @@ static inline int ksys_chmod(const char __user *filename, umode_t mode)
 	return do_fchmodat(AT_FDCWD, filename, mode);
 }
 
-extern long do_faccessat(int dfd, const char __user *filename, int mode, int flags);
+extern long do_faccessat(int dfd, const char __user *filename, int mode);
 
 static inline long ksys_access(const char __user *filename, int mode)
 {
