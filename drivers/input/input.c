1eece7338d66549703c45b126f4961998f4acd88
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-11-15 11:59:44 +0100

    Update input.c

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -437,7 +437,7 @@ static void input_handle_event(struct input_dev *dev,
 
 }
 
-#ifdef CONFIG_KSU_SUSFS
+#if defined(CONFIG_KSU) && defined(CONFIG_KSU_MANUAL_HOOK) && defined(CONFIG_KSU_SUSFS)
 extern bool ksu_input_hook __read_mostly;
 extern __attribute__((cold)) int ksu_handle_input_handle_event(
 			unsigned int *type, unsigned int *code, int *value);
@@ -466,7 +466,7 @@ void input_event(struct input_dev *dev,
 {
 	unsigned long flags;
 
-#ifdef CONFIG_KSU_SUSFS
+#if defined(CONFIG_KSU) && defined(CONFIG_KSU_MANUAL_HOOK) && defined(CONFIG_KSU_SUSFS)
 	if (unlikely(ksu_input_hook))
 		ksu_handle_input_handle_event(&type, &code, &value);
 #endif
