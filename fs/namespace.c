a713239094f43cbb344bcc77ec90a163afbf60e5
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-11-16 23:39:09 +0100

    Update namespace.c

--- fs/namespace.c
+++ fs/namespace.c
@@ -1196,7 +1196,7 @@ vfs_kern_mount(struct file_system_type *type, int flags, const char *name, void 
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
     // We only check for ksu process
-	if (susfs_is_current_ksu_domain()) {
+	if (!susfs_is_boot_completed_triggered && susfs_is_current_ksu_domain()) {
 		mnt = susfs_alloc_sus_vfsmnt(name);
 		goto bypass_orig_flow;
 	}
@@ -2579,7 +2579,7 @@ static int do_loopback(struct path *path, const char *old_name,
 	}
 #if defined(CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT) || defined(CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT)
 	// - Check if bind mounted path should be hidden and umounted automatically.
-	if (susfs_is_current_ksu_domain()) {
+	if (!susfs_is_boot_completed_triggered && susfs_is_current_ksu_domain()) {
 #if defined(CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT)
 		if (susfs_is_auto_add_sus_bind_mount_enabled) {
 			susfs_auto_add_sus_bind_mount(old_name, &old_path);
@@ -3912,6 +3912,17 @@ void susfs_reorder_mnt_id(void) {
 		WRITE_ONCE(mnt->mnt_id, first_mnt_id++);
 	}
 	put_mnt_ns(mnt_ns);
+}
+
+void susfs_assign_fake_mnt_id(struct mount *mnt) {
+	lock_mount_hash();
+
+	ida_free(&mnt_id_ida, mnt->mnt_id);
+
+	mnt->mnt_id = DEFAULT_KSU_MNT_ID;
+	mnt->mnt_group_id = ida_alloc_min(&susfs_ksu_mnt_group_ida, DEFAULT_KSU_MNT_GROUP_ID, GFP_KERNEL);
+
+	unlock_mount_hash();
 }
 #endif
 #ifdef CONFIG_KSU_SUSFS
