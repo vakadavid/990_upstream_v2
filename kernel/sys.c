5cd391b30b156952f1724577b89d17e9c21b10ca
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-11-17 22:44:37 +0100

    Update sys.c

--- kernel/sys.c
+++ kernel/sys.c
@@ -1272,6 +1272,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+	sprintf(tmp.release, "5.10.245%s", CONFIG_LOCALVERSION);
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 	susfs_spoof_uname(&tmp);
 #endif
