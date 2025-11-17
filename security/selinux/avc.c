dac2e360605ad1675f0e79da54daa432db3399c0
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-11-17 18:45:48 +0100

    Update avc.c

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -195,7 +195,7 @@ static void avc_dump_query(struct audit_buffer *ab, struct selinux_state *state,
 
 	rc = security_sid_to_context(state, tsid, &scontext, &scontext_len);
 #ifdef CONFIG_KSU_SUSFS
-	if (unlikely(sad->tsid == susfs_ksu_sid && susfs_is_avc_log_spoofing_enabled)) {
+	if (unlikely(tsid == susfs_ksu_sid && susfs_is_avc_log_spoofing_enabled)) {
 		if (rc)
 			audit_log_format(ab, " tsid=%d", susfs_priv_app_sid);
 		else
