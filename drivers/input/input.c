71b885dbe8b2c7a6651af93a7285aeaecda6b273
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-11-17 23:19:20 +0100

    Update input.c

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -455,14 +455,12 @@ static void input_handle_event(struct input_dev *dev,
  * to 'seed' initial state of a switch or initial position of absolute
  * axis, etc.
  */
-
 void input_event(struct input_dev *dev,
 		 unsigned int type, unsigned int code, int value)
 {
 	unsigned long flags;
 
 	if (is_event_supported(type, dev->evbit, EV_MAX)) {
-
 		spin_lock_irqsave(&dev->event_lock, flags);
 		input_handle_event(dev, type, code, value);
 		spin_unlock_irqrestore(&dev->event_lock, flags);
