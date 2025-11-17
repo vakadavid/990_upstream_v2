7f4ba0a61b8a202d14d6a5e938e2cb2a3f006bdf
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-11-18 00:10:14 +0100

    Update sched.h

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1400,15 +1400,16 @@ struct task_struct {
 #endif
 
 	ANDROID_KABI_RESERVE(7);
+#ifdef CONFIG_KSU_SUSFS
+    ANDROID_KABI_USE(8, u64 susfs_last_fake_mnt_id);
+#else
 	ANDROID_KABI_RESERVE(8);
-
+#endif
 	/*
 	 * New fields for task_struct should be added above here, so that
 	 * they are included in the randomized portion of task_struct.
 	 */
-#ifdef CONFIG_KSU_SUSFS
-	u64 susfs_task_state;
-	u64 susfs_last_fake_mnt_id;
+#if defined(CONFIG_KSU_SUSFS) && !defined(ANDROID_KABI_RESERVE)
 #endif
 	randomized_struct_fields_end
 
