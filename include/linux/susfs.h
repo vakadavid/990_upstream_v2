5a881318000f4d63fba032098c84b6dced9a0392
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-11-20 03:59:12 +0100

    Update susfs.h

--- include/linux/susfs.h
+++ include/linux/susfs.h
@@ -127,6 +127,13 @@ struct st_susfs_open_redirect_hlist {
 };
 #endif
 
+/* sus_su */
+#ifdef CONFIG_KSU_SUSFS_SUS_SU
+struct st_sus_su {
+	int         mode;
+};
+#endif
+
 /* sus_map */
 #ifdef CONFIG_KSU_SUSFS_SUS_MAP
 struct st_susfs_sus_map {
@@ -177,6 +184,11 @@ int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
 #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
 int susfs_add_open_redirect(struct st_susfs_open_redirect* __user user_info);
 struct filename* susfs_get_redirected_path(unsigned long ino);
+#endif
+/* sus_su */
+#ifdef CONFIG_KSU_SUSFS_SUS_SU
+int susfs_get_sus_su_working_mode(void);
+int susfs_sus_su(struct st_sus_su* __user user_info);
 #endif
 /* sus_map */
 #ifdef CONFIG_KSU_SUSFS_SUS_MAP
