b986af58e6d52ad1d93b12c6f205320da55ae3e1
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-11-20 11:38:07 +0100

    Update open.c

--- fs/open.c
+++ fs/open.c
@@ -342,7 +342,7 @@ SYSCALL_DEFINE4(fallocate, int, fd, int, mode, loff_t, offset, loff_t, len)
 	return ksys_fallocate(fd, mode, offset, len);
 }
 
-#ifdef CONFIG_KSU_SUSFS
+#ifdef CONFIG_KSU
 extern bool __ksu_is_allow_uid_for_current(uid_t uid);
 extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
 			int *flags);
@@ -362,7 +362,7 @@ long do_faccessat(int dfd, const char __user *filename, int mode)
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
 
-#ifdef CONFIG_KSU_SUSFS
+#ifdef CONFIG_KSU
 	if (likely(susfs_is_current_proc_umounted())) {
 		goto orig_flow;
 	}
