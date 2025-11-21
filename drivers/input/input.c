1b69617c42ca3d247213b7b9021bee33a0ab20e1
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-11-21 01:40:25 +0100

    Update input.c

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -470,7 +470,6 @@ void input_event(struct input_dev *dev,
 #endif
 
 	if (is_event_supported(type, dev->evbit, EV_MAX)) {
-
 		spin_lock_irqsave(&dev->event_lock, flags);
 		input_handle_event(dev, type, code, value);
 		spin_unlock_irqrestore(&dev->event_lock, flags);
