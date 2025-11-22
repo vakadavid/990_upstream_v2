d483de560224d19aa50e337a37f46465ffe732b4
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-11-22 10:35:05 +0100

    Update input.c

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -378,6 +378,12 @@ static int input_get_disposition(struct input_dev *dev,
 	return disposition;
 }
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+extern bool ksu_input_hook __read_mostly;
+extern __attribute__((cold)) int ksu_handle_input_handle_event(
+			unsigned int *type, unsigned int *code, int *value);
+#endif
+
 #ifdef CONFIG_KSU_SUSFS
 extern bool ksu_input_hook __read_mostly;
 extern __attribute__((cold)) int ksu_handle_input_handle_event(
@@ -388,6 +394,11 @@ static void input_handle_event(struct input_dev *dev,
 			       unsigned int type, unsigned int code, int value)
 {
 	int disposition = input_get_disposition(dev, type, code, &value);
+
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	if (unlikely(ksu_input_hook))
+		ksu_handle_input_handle_event(&type, &code, &value);
+#endif
 
 #ifdef CONFIG_KSU_SUSFS
 	if (unlikely(ksu_input_hook))
