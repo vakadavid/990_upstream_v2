b03c6447e00540d598048e0475fc8e6e26594516
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-11-24 12:42:07 +0100

    Update avc.c

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -198,8 +198,10 @@ static void avc_dump_query(struct audit_buffer *ab, struct selinux_state *state,
 	if (unlikely(tsid == susfs_ksu_sid && susfs_is_avc_log_spoofing_enabled)) {
 		if (rc)
 			audit_log_format(ab, " tsid=%d", susfs_priv_app_sid);
-		else
-			audit_log_format(ab, " tcontext=%s", "u:r:priv_app:s0:c512,c768");
+		else {
+			audit_log_format(ab, " tcontext=%s", scontext);
+			kfree(scontext);
+		}
 		goto bypass_orig_flow;
 	}
 #endif
