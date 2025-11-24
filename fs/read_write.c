36ebdc17d75163f09a275993f911b482a870a3f0
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-11-24 12:17:34 +0100

    Update read_write.c

--- fs/read_write.c
+++ fs/read_write.c
@@ -594,13 +594,7 @@ ssize_t ksys_read(unsigned int fd, char __user *buf, size_t count)
 	return ret;
 }
 
-#ifdef CONFIG_KSU_MANUAL_HOOK
-extern bool ksu_vfs_read_hook __read_mostly;
-extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,
-				char __user **buf_ptr, size_t *count_ptr);
-#endif
-
-#if defined(CONFIG_KSU) && defined(CONFIG_KSU_SUSFS)
+#if defined(CONFIG_KSU_SUSFS) && defined(CONFIG_KSU_MANUAL_HOOK)
 extern bool ksu_vfs_read_hook __read_mostly;
 extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,
                char __user **buf_ptr, size_t *count_ptr);
@@ -608,16 +602,10 @@ extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,
 
 SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 {
-#ifdef CONFIG_KSU_MANUAL_HOOK
+#if defined(CONFIG_KSU_SUSFS) && defined(CONFIG_KSU_MANUAL_HOOK)
 	if (unlikely(ksu_vfs_read_hook))
 		ksu_handle_sys_read(fd, &buf, &count);
 #endif
-
-#if defined(CONFIG_KSU) && defined(CONFIG_KSU_SUSFS)
-	if (unlikely(ksu_vfs_read_hook))
-		ksu_handle_sys_read(fd, &buf, &count);
-#endif
-
 	return ksys_read(fd, buf, count);
 }
 
