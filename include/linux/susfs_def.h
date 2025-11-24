f003d0d2674ea6c2d30810afa65a49b64bc1f11a
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-11-24 11:09:56 +0100

    Update susfs_def.h

--- include/linux/susfs_def.h
+++ include/linux/susfs_def.h
@@ -2,6 +2,7 @@
 #define KSU_SUSFS_DEF_H
 
 #include <linux/bits.h>
+#include <linux/version.h>
 
 /********/
 /* ENUM */
@@ -46,14 +47,18 @@
 
 /*
  * mount->mnt.susfs_mnt_id_backup => storing original mount's mnt_id
- * inode->i_mapping->flags => A 'unsigned long' type storing flag 'AS_FLAGS_', bit 1 to 31 is not usable since 6.12
+ * inode->i_mapping->flags => A 'unsigned long' type storing flag 'AS_FLAGS_'
  * nd->state => storing flag 'ND_STATE_'
  * nd->flags => storing flag 'ND_FLAGS_'
  * task_struct->thread_info.flags => storing flag 'TIF_'
+ *
+ * NOTE: For kernel 4.14, we keep AS_FLAGS at bit 24-30 since i_mapping->flags
+ * is 32-bit. For kernel 6.12+, these should be 33-39.
  */
  // thread_info->flags is unsigned long :D
 #define TIF_PROC_UMOUNTED 33
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 12, 0)
 #define AS_FLAGS_SUS_PATH 33
 #define AS_FLAGS_SUS_MOUNT 34
 #define AS_FLAGS_SUS_KSTAT 35
@@ -68,12 +73,28 @@
 #define BIT_ANDROID_DATA_ROOT_DIR BIT(37)
 #define BIT_ANDROID_SDCARD_ROOT_DIR BIT(38)
 #define BIT_SUS_MAPS BIT(39)
+#else
+#define AS_FLAGS_SUS_PATH 24
+#define AS_FLAGS_SUS_MOUNT 25
+#define AS_FLAGS_SUS_KSTAT 26
+#define AS_FLAGS_OPEN_REDIRECT 27
+#define AS_FLAGS_ANDROID_DATA_ROOT_DIR 28
+#define AS_FLAGS_SDCARD_ROOT_DIR 29
+#define AS_FLAGS_SUS_MAP 30
+#define BIT_SUS_PATH BIT(24)
+#define BIT_SUS_MOUNT BIT(25)
+#define BIT_SUS_KSTAT BIT(26)
+#define BIT_OPEN_REDIRECT BIT(27)
+#define BIT_ANDROID_DATA_ROOT_DIR BIT(28)
+#define BIT_ANDROID_SDCARD_ROOT_DIR BIT(29)
+#define BIT_SUS_MAPS BIT(30)
+#endif
 
 #define ND_STATE_LOOKUP_LAST 32
 #define ND_STATE_OPEN_LAST 64
 #define ND_STATE_LAST_SDCARD_SUS_PATH 128
 #define ND_FLAGS_LOOKUP_LAST		0x2000000
- 
+
 #define MAGIC_MOUNT_WORKDIR "/debug_ramdisk/workdir"
 
 static inline bool susfs_is_current_proc_umounted(void) {
