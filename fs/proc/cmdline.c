32f3b8590820623e0c613c907f010366d6337b13
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-11-25 09:24:59 +0100

    Update cmdline.c

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -12,16 +12,15 @@ enum {
 
 static char new_command_line[COMMAND_LINE_SIZE];
 
-#ifdef CONFIG_KSU_SUSFS_SUS_PROC_CMDLINE
-#include <linux/susfs_def.h>
-extern bool susfs_is_current_proc_umounted(void);
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
 #endif
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-#ifdef CONFIG_KSU_SUSFS_SUS_PROC_CMDLINE
-	if (likely(susfs_is_current_proc_umounted())) {
-		seq_printf(m, "%s\n", SUSFS_FAKE_PROC_CMDLINE);
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+	if (!susfs_spoof_cmdline_or_bootconfig(m)) {
+		seq_putc(m, '\n');
 		return 0;
 	}
 #endif
