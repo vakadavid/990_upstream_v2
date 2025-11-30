2272d081b386eaf0f379b7083a4cedc177585ccf
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-11-30 12:05:31 +0100

    Update reboot.c

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -304,7 +304,7 @@ EXPORT_SYMBOL_GPL(kernel_power_off);
 
 DEFINE_MUTEX(system_transition_mutex);
 
-#ifdef CONFIG_KSU_SUSFS
+#ifdef CONFIG_KSU
 extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
 #endif
 /*
