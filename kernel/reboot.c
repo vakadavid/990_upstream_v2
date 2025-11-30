d872cfb19ba87ad6801769dd590e08a8bcce1cb3
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-11-30 12:19:49 +0100

    Update reboot.c

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -304,9 +304,6 @@ EXPORT_SYMBOL_GPL(kernel_power_off);
 
 DEFINE_MUTEX(system_transition_mutex);
 
-#ifdef CONFIG_KSU
-extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
-#endif
 /*
  * Reboot system call: for obvious reasons only root may call it,
  * and even root needs to set up some magic numbers in the registers
@@ -315,6 +312,9 @@ extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void 
  *
  * reboot doesn't sync: do that yourself before calling this.
  */
+#ifdef CONFIG_KSU
+extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
+#endif
 SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 		void __user *, arg)
 {
