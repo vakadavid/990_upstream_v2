8dc441eddf65683a6c1c6a2553d04a093200f79c
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-12-04 14:00:47 +0100

    Refactor KSU hook conditionals in read_write.c

--- fs/read_write.c
+++ fs/read_write.c
@@ -594,10 +594,10 @@ ssize_t ksys_read(unsigned int fd, char __user *buf, size_t count)
 	return ret;
 }
 
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_MANUAL_HOOK
 extern bool ksu_vfs_read_hook __read_mostly;
-extern int ksu_handle_sys_read(unsigned int fd,
-               char __user **buf_ptr, size_t *count_ptr);
+extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,
+				char __user **buf_ptr, size_t *count_ptr);
 #endif
 
 #ifdef CONFIG_KSU_SUSFS
@@ -608,13 +608,11 @@ extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,
 
 SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 {
-
-#ifdef CONFIG_KSU 
+#ifdef CONFIG_KSU_SUSFS
 	if (unlikely(ksu_vfs_read_hook))
 		ksu_handle_sys_read(fd, &buf, &count);
 #endif
-
-#ifdef CONFIG_KSU_SUSFS
+#ifdef CONFIG_KSU_MANUAL_HOOK
 	if (unlikely(ksu_vfs_read_hook))
 		ksu_handle_sys_read(fd, &buf, &count);
 #endif
