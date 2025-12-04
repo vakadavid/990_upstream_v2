a01b96ff68ca7ca18d5c336932976eddb4405474
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-12-04 20:21:33 +0100

    Update reboot.c

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -313,7 +313,7 @@ DEFINE_MUTEX(system_transition_mutex);
  * reboot doesn't sync: do that yourself before calling this.
  */
 
-#if defined(CONFIG_KSU_MANUAL_HOOK) && !defined(CONFIG_KSU_NEXT)
+#ifdef CONFIG_KSU_MANUAL_HOOK
 extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
 #endif
 
@@ -328,7 +328,7 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 	char buffer[256];
 	int ret = 0;
 
-#if defined(CONFIG_KSU_MANUAL_HOOK) && !defined(CONFIG_KSU_NEXT)
+#ifdef CONFIG_KSU_MANUAL_HOOK
 	ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
 #endif
 
