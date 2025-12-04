caeb3f825ea3d8c3c3af287f7ae66d1af8ff5193
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-12-04 14:13:51 +0100

    Modify reboot handling for KSU configuration

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -313,7 +313,7 @@ DEFINE_MUTEX(system_transition_mutex);
  * reboot doesn't sync: do that yourself before calling this.
  */
 
-#ifdef CONFIG_KSU
+#if defined(CONFIG_KSU_MANUAL_HOOK) && !defined(CONFIG_KSU_NEXT)
 extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
 #endif
 
@@ -328,8 +328,8 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 	char buffer[256];
 	int ret = 0;
 
-#ifdef CONFIG_KSU
-    ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
+#if defined(CONFIG_KSU_MANUAL_HOOK) && !defined(CONFIG_KSU_NEXT)
+	ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
 #endif
 
 #ifdef CONFIG_KSU_SUSFS
