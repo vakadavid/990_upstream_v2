ff8f402fdb9d9d24bb9fc0469feb5efc48faf6ac
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-12-05 11:39:14 +0100

    Update read_write.c

--- fs/read_write.c
+++ fs/read_write.c
@@ -600,7 +600,7 @@ extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,
 				char __user **buf_ptr, size_t *count_ptr);
 #endif
 
-#ifdef CONFIG_KSU_SUSFS
+#ifdef CONFIG_KSU
 extern bool ksu_vfs_read_hook __read_mostly;
 extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,
                char __user **buf_ptr, size_t *count_ptr);
@@ -612,7 +612,7 @@ SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 	if (unlikely(ksu_vfs_read_hook))
 		ksu_handle_sys_read(fd, &buf, &count);
 #endif
-#ifdef CONFIG_KSU_SUSFS
+#ifdef CONFIG_KSU
 	if (unlikely(ksu_vfs_read_hook))
 		ksu_handle_sys_read(fd, &buf, &count);
 #endif
