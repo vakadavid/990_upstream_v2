83456fb718bed66ee10e69caf837dd39b12f8f2e
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-12-05 03:29:33 +0100

    Update inode.c

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -565,10 +565,6 @@ void devpts_kill_index(struct pts_fs_info *fsi, int idx)
  *
  * The created inode is returned. Remove it from /dev/pts/ by devpts_pty_kill.
  */
-#ifdef CONFIG_KSU_SUSFS
-extern int ksu_handle_devpts(struct inode*);
-#endif
-
 struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
 {
 	struct dentry *dentry;
@@ -605,6 +601,10 @@ struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
 
 	return dentry;
 }
+
+#ifdef CONFIG_KSU_SUSFS
+extern int ksu_handle_devpts(struct inode*);
+#endif
 
 /**
  * devpts_get_priv -- get private data for a slave
