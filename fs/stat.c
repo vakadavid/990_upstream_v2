b02ab140fd2cb1d846566c71ee793ca7bd149507
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-12-07 23:36:18 +0100

    Update stat.c

--- fs/stat.c
+++ fs/stat.c
@@ -397,7 +397,7 @@ SYSCALL_DEFINE2(newlstat, const char __user *, filename,
 	return cp_new_stat(&stat, statbuf);
 }
 
-#ifdef CONFIG_KSU_MANUAL_HOOK
+#ifdef CONFIG_SAJTBI_MANUAL_HOOK
 __attribute__((hot))
 extern int ksu_handle_stat(int *dfd, const char __user **filename_user,
 				int *flags);
@@ -410,7 +410,7 @@ SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 	struct kstat stat;
 	int error;
 
-#ifdef CONFIG_KSU_MANUAL_HOOK
+#ifdef CONFIG_SAJTBI_MANUAL_HOOK
 	ksu_handle_stat(&dfd, &filename, &flag);
 #endif
 
@@ -564,7 +564,7 @@ SYSCALL_DEFINE4(fstatat64, int, dfd, const char __user *, filename,
 	struct kstat stat;
 	int error;
 
-#ifdef CONFIG_KSU_MANUAL_HOOK // 32-bit su
+#ifdef CONFIG_SAJTBI_MANUAL_HOOK // 32-bit su
 	ksu_handle_stat(&dfd, &filename, &flag);
 #endif
 
