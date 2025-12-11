9d316df9b8c10c92d6d578e4bb4c7c3a985f0234
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-12-11 10:19:21 +0100

    Update is-binary.c

--- drivers/media/platform/exynos/camera/is-binary.c
+++ drivers/media/platform/exynos/camera/is-binary.c
@@ -82,7 +82,7 @@ static noinline_for_stack long get_file_size(struct file *file)
 	struct kstat st;
 	u32 request_mask = (STATX_MODE | STATX_SIZE);
 
-	if (vfs_getattr(&file->f_path, &st, request_mask, KSTAT_QUERY_FLAGS))
+	if (vfs_getattr(&file->f_path, &st, request_mask, AT_STATX_SYNC_TYPE))
 		return -1;
 	if (!S_ISREG(st.mode))
 		return -1;
