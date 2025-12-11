ad9e4b0336f1aa2e0918c72e5c0020f45d80b860
vakadavid <134170125+vakadavid@users.noreply.github.com>
2025-12-11 11:20:45 +0100

    Update namespace.c

--- fs/namespace.c
+++ fs/namespace.c
@@ -3395,12 +3395,6 @@ out_type:
 	return ret;
 }
 
-SYSCALL_DEFINE5(mount, char __user *, dev_name, char __user *, dir_name,
-		char __user *, type, unsigned long, flags, void __user *, data)
-{
-	return ksys_mount(dev_name, dir_name, type, flags, data);
-}
-
 /*
  * Move a mount from one place to another.
  *
