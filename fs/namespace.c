58c2899a10f8abd130cb1f31146b3f4c16431493
vakadavid <134170125+vakadavid@users.noreply.github.com>
2026-01-13 09:48:56 +0100

    Update namespace.c

--- fs/namespace.c
+++ fs/namespace.c
@@ -1676,7 +1676,7 @@ static void namespace_unlock(void)
 	if (likely(hlist_empty(&head)))
 		return;
 
-	synchronize_rcu_expedited();
+	synchronize_rcu();
 
 	group_pin_kill(&head);
 }
@@ -3744,7 +3744,7 @@ void kern_unmount(struct vfsmount *mnt)
 	/* release long term mount so mount point can be released */
 	if (!IS_ERR_OR_NULL(mnt)) {
 		real_mount(mnt)->mnt_ns = NULL;
-		synchronize_rcu_expedited();	/* yecchhh... */
+		synchronize_rcu();	/* yecchhh... */
 		mntput(mnt);
 	}
 }
