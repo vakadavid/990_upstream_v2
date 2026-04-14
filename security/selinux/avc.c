ad6dd5bece8860c041261ce37723e2c3e4bab748
vakadavid <134170125+vakadavid@users.noreply.github.com>
2026-04-14 04:20:20 +0200

    testttttt

--- security/selinux/avc.c
+++ security/selinux/avc.c
@@ -183,6 +183,10 @@ static void avc_dump_query(struct audit_buffer *ab, struct selinux_state *state,
 	int rc;
 	char *scontext;
 	u32 scontext_len;
+    
+#ifdef CONFIG_KSU_SUSFS
+    struct selinux_audit_data sad;
+#endif
 
 	rc = security_sid_to_context(state, ssid, &scontext, &scontext_len);
 	if (rc)
@@ -193,17 +197,17 @@ static void avc_dump_query(struct audit_buffer *ab, struct selinux_state *state,
 	}
 
 	rc = security_sid_to_context(state, tsid, &scontext, &scontext_len);
+
 #ifdef CONFIG_KSU_SUSFS
-	if (unlikely(tsid == susfs_ksu_sid && READ_ONCE(susfs_is_avc_log_spoofing_enabled))) {
+	if (unlikely(sad.tsid == susfs_ksu_sid && READ_ONCE(susfs_is_avc_log_spoofing_enabled))) {
 		if (rc)
 	    	audit_log_format(ab, " tsid=%d", susfs_priv_app_sid);
-		else {
+		else
 	    	audit_log_format(ab, " tcontext=%s", "u:r:priv_app:s0:c512,c768");
-	    	kfree(scontext);
-		}
 		goto bypass_orig_flow;
 	}
 #endif
+
 	if (rc)
 		audit_log_format(ab, " tsid=%d", tsid);
 	else {
