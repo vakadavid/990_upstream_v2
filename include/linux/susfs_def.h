d3d0d23a2bd9a70f62969cb9f3cc4268f479dab8
vakadavid <134170125+vakadavid@users.noreply.github.com>
2026-04-27 22:20:56 +0200

    Use test_thread_flag() and set_thread_flag() instead of test_ti_thread_flag() and set_ti_thread_flag()

--- include/linux/susfs_def.h
+++ include/linux/susfs_def.h
@@ -135,15 +135,15 @@ int data_type, susfs_fname_t file_name, u32 cookie)
 #endif
 
 static inline bool susfs_is_current_proc_umounted(void) {
-	return test_ti_thread_flag(&current->thread_info, TIF_PROC_UMOUNTED);
+	return (likely(test_thread_flag(TIF_PROC_UMOUNTED)));
 }
 
 static inline void susfs_set_current_proc_umounted(void) {
-	set_ti_thread_flag(&current->thread_info, TIF_PROC_UMOUNTED);
+	set_thread_flag(TIF_PROC_UMOUNTED);
 }
 
 static inline bool susfs_is_current_proc_umounted_app(void) {
-	return (test_ti_thread_flag(&current->thread_info, TIF_PROC_UMOUNTED) &&
+	return (likely(test_thread_flag(TIF_PROC_UMOUNTED)) &&
 			current_uid().val >= 10000);
 }
 
